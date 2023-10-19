b1c3e6506c5950b086a522466b4601c27ffd2489
Kevin <hernandezkaleth647@gmail.com>
2023-10-19 11:40:54 -0500

    Añadí las carpetas y el contenido necesario, hice el JavaScript correspondiente

---
+++ game.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
+
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
+}
